eb3d3869d1261772779350bede2a49279fc5f1f8
rlacodinglife <rlacodinglife@gmail.com>
2024-03-19 17:04:28 +0900

    240319_1704

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -16,8 +16,8 @@ export default function Footer() {
 
                 </div>
                 <div className={style.footer_eig}>
-                    <p style={{ color: '#FFFFFF' }}>(우)04821 서울특별시 성북구 북악산로1다길 47 / Tex :(010)9415-9609</p>
-                    <p style={{ color: '#FFFFFF' }}> Email :rla960926@gmail.com</p>
+                    <p style={{ color: '#FFFFFF' }}>(우)04821 서울특별시 종로구  / Tex :(010)0000-0000</p>
+                    <p style={{ color: '#FFFFFF' }}> Email :rlacodinglife@gmail.com</p>
                     <p style={{ color: '#FFFFFF' }}>insta: @my_life.ssul</p>
                 </div>
                 {/* <button className={style.darkMode} onClick={toggleDarkMode}>
