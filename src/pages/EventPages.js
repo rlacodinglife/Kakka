ede159074d1054758bfc3b3a90eef74b701ce0fb
rlacodinglife <rlacodinglife@gmail.com>
2024-03-25 18:21:42 +0900

    240325_1821

--- src/pages/EventPages.js
+++ src/pages/EventPages.js
@@ -1,17 +1,9 @@
 import React from 'react'
-import EventVisual from '../Component/EventPage/EventVisual'
-import EventBoard from '../Component/EventPage/EventBorad'
 
-
-
-
-
-export default function EventPage() {
+export default function EventPages() {
   return (
-    <>
-      <EventVisual />
-      <EventBoard />
-    </>
-    
+   <>
+   {/* <EventVisual/> */}
+   </>
   )
 }
